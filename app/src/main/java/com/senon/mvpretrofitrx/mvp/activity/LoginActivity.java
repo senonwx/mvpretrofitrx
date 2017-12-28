36e7f0fc63aec5d56a1e757dca9b2f3eaacfd348
senonwx <1083911695@qq.com>
2017-12-28 16:33:27 +0800

    second commit -rename

--- app/src/main/java/com/senon/mvpretrofitrx/mvp/activity/LoginActivity.java
+++ app/src/main/java/com/senon/mvpretrofitrx/mvp/activity/LoginActivity.java
@@ -47,6 +47,7 @@ public class LoginActivity extends BaseActivity<LoginContract.View, LoginContrac
                 beginTransaction().
                 replace(R.id.frame_lay, new LoginFragment()).
                 commitAllowingStateLoss();
+
     }
 
     @Override
